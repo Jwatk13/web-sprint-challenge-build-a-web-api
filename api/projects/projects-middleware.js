8d42728296a9d29c3e0df5de7de8a08669cda7e0
Joseph Watkins <josephxwatkins@gmail.com>
2022-07-15 15:57:43 -0400

    Projects middleware added

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,29 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+    let id = req.params.id;
+    let project = await Projects.get(id)
+    if (!project) {
+        res.status(404).json({ message: "Project not found" });
+        return;
+    }
+    req.project = project
+    next();
+}
+
+function validateProject(req, res, next) {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ message: "Requires both name and description fields to be filled out" });
+        return;
+    }
+    req.name = {
+        name: req.body.name.trim()
+    };
+    req.description = {
+        description: req.body.description
+    };
+    next();
+}
+
+module.exports = { validateProjectId, validateProject };
